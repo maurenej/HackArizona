1307a07604410d643bd6ec0f9afeabbac027697e
Maurene Jiang <maurenej@dhcp-10-134-229-98.uawifi.arizona.edu>
2018-01-13 15:43:06 -0700

    Bug fixes

--- face.py
+++ face.py
@@ -57,7 +57,7 @@ start_time = 0
 EYE_AR_THRESH = 0.24
 EYE_AR_INC_THRESH = 0.1
 EYE_AR_CONSEC_FRAMES = 3
-BPM_THRESH = 10
+BPM_THRESH = 100
  
 # initialize the frame counters and the total number of blinks
 COUNTER = 0
